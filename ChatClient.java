3ab178ee2e47eafd96c65b9c0419f8122237b419
Major Ursa <grayjay@majorursa.net>
2012-02-19 13:17:01 -0700

    updated padding and added textField.requestFocus()

--- ChatClient.java
+++ ChatClient.java
@@ -6,24 +6,22 @@ import java.net.*;
 import java.util.*;
 
 public class ChatClient extends JPanel implements ActionListener {
-    protected JTextField textField;
+    private final static int port = 1337;
+    private final static String host = "localhost";
+
+    protected static JTextField textField;
     public static JTextArea textArea;
     private final static String newline = "\n";
     private Socket csock;
     protected static Queue<String> outMsg;
-    private final static String host = "localhost";
-    private final static int port = 1337;
     private static ChatClientConnection cc;
-
+    
     public ChatClient() {
         super(new GridBagLayout());
 
-        // outgoing messages
-        //outMsg = new LinkedList<String>();
-        //outMsg.add("Welcome to Ursa Major!");
         System.out.println("size of queue: " + outMsg.size());        
         // Add a label 
-        JLabel label = new JLabel("Welcome to Ursa Major, Cadet!");
+        JLabel label = new JLabel("Welcome to Ursa Major Chatroom, Cadet!");
 
         textArea = new JTextArea(10,40);
         textArea.setEditable(false);
@@ -47,15 +45,21 @@ public class ChatClient extends JPanel implements ActionListener {
         GridBagConstraints c = new GridBagConstraints();
         c.gridwidth = GridBagConstraints.REMAINDER;
         
-        c.fill = GridBagConstraints.HORIZONTAL;
+        c.fill = GridBagConstraints.CENTER;
+        c.ipady=20;
+        c.ipadx=10;
         add(label, c);
 
         c.fill = GridBagConstraints.BOTH;
+        c.ipady = 10;
+        c.ipadx = 0;
         c.weightx = 1.0;
         c.weighty = 1.0;
         add(scrollPane, c);
         
         c.fill = GridBagConstraints.HORIZONTAL;
+        c.ipady=4;
+        c.ipadx=2;
         add(textField, c);
 
     }
@@ -66,14 +70,14 @@ public class ChatClient extends JPanel implements ActionListener {
     private static void createAndShowGUI() {
         JFrame frame = new JFrame("Chat Room of the Geeks!");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
         frame.add(new ChatClient());
 
         // display the window
         frame.pack();
         frame.setVisible(true);
+        // grab focus for the textField
+        textField.requestFocus();
     }
-    
     
     public static void main(String[] args) {
         outMsg = new LinkedList<String>();
@@ -83,11 +87,10 @@ public class ChatClient extends JPanel implements ActionListener {
             }
         });
 
-        //System.out.println("size of queue: " + outMsg.size());
         cc = new ChatClientConnection(outMsg);
         cc.chatLoop();   
-    
     }
+
     protected static class ChatClientConnection {
         // Network Connections
         private Socket clientSocket;
@@ -97,7 +100,6 @@ public class ChatClient extends JPanel implements ActionListener {
         private String outs;
         private Queue<String> outMsg;
     
-
         public ChatClientConnection(Queue<String> outQ) {
             try {
                 //brin = new BufferedReader(new InputStreamReader(System.in));
@@ -116,10 +118,9 @@ public class ChatClient extends JPanel implements ActionListener {
         public void chatLoop() {
             String msg;
             do {
-                //sleep(10);
                 //synchronized(outMsg) {
                 msg = outMsg.poll();
-                    //}
+                //}
           
                 try {
                     if (msg != null) {
@@ -131,7 +132,9 @@ public class ChatClient extends JPanel implements ActionListener {
                         received = new String(buf);
                         javax.swing.SwingUtilities.invokeLater(new Runnable() {
                                 public void run() {
-                                    textArea.append(received);
+                                    synchronized (outMsg) {
+                                        textArea.append(received);
+                                    }
                                 }
                             });
                     } 
@@ -144,34 +147,3 @@ public class ChatClient extends JPanel implements ActionListener {
 
     
 }
-
-
-// class SocketWorker extends SwingWorker<Void,String> {
-//     @Override
-//     public void doInBackground()   {
-//         private Socket clientSocket;
-//         private PrintWriter outToServer;
-//         private InputStream inputStream;
-//         private String received;
-//         private String outs;
-//         //private Queue<String> outMsg = new LinkedList<;
-        
-//         try {
-//             //brin = new BufferedReader(new InputStreamReader(System.in));
-
-//             clientSocket = new Socket("localhost", 1337);
-//             outToServer = new PrintWriter(clientSocket.getOutputStream(), true);
-//             inputStream = clientSocket.getInputStream();
-//             System.out.println("Connected");
-//         } catch (Exception e) {
-//             e.printStackTrace();
-//         }
-//     }
-
-//     @Override
-//     protected void process (List<String> chunks) {
-//         for(String message : chunks) {
-//             textArea.append(message);
-//         }
-//     }
-// }
